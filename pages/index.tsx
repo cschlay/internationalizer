bde94d939d473ebfafe394f4a725a17b62c20c30
Chi-Hao Lay <chi-hao.lay@outlook.com>
2021-08-16 10:23:55 +0300

    Add warning for pending changes

---
+++ pages/index.tsx
@@ -0,0 +1,104 @@
+import Head from "next/head";
+import { useCallback, useEffect, useState } from "react";
+import { EditTable } from "../components/EditTable";
+import { toTSX } from "../utils/toTSX";
+import { FileListing } from "../components/FileListing";
+import { ToolBar } from "../components/ToolBar";
+import { TranslationFileContent } from "../types";
+
+const HomePage = () => {
+  const [file, setFile] = useState<TranslationFileContent>({
+    name: "",
+    translations: {},
+    path: "",
+    relativePath: "",
+  });
+  const [hasPendingChanges, setHasPendingChanges] = useState<boolean>(false);
+
+  const handleFileSelected = (filepath) => {
+    if (!hasPendingChanges || window.confirm("You have unsaved changes.")) {
+      fetch(`/api/get-file-content?file=${encodeURIComponent(filepath)}`)
+        .then((res) => res.json())
+        .then(setFile)
+        .then(() => setHasPendingChanges(false));
+    }
+  };
+
+  const handleChange = (locale: string, key: string, value: string) => {
+    setHasPendingChanges(true);
+    const updatedFile: TranslationFileContent = { ...file };
+    updatedFile.translations[key][locale] = value;
+    setFile(updatedFile);
+  };
+
+  const handleSave = useCallback(() => {
+    if (hasPendingChanges) {
+      fetch("/api/write-changes", {
+        method: "POST",
+        body: JSON.stringify({ path: file.path, content: toTSX(file) }),
+      }).then(() => setHasPendingChanges(false));
+    }
+  }, [file, hasPendingChanges]);
+
+  useEffect(() => {
+    if (hasPendingChanges) {
+      window.addEventListener("beforeunload", handlePendingChanges);
+    }
+    return () =>
+      window.removeEventListener("beforeunload", handlePendingChanges);
+  }, [hasPendingChanges]);
+
+  useEffect(() => {
+    const saveEventHandler = (event: KeyboardEvent) => {
+      if (event.ctrlKey && event.key === "s") {
+        // Default CTRL + S is to save page.
+        event.preventDefault();
+        handleSave();
+      }
+    };
+    window.addEventListener("keydown", saveEventHandler);
+    return () => window.removeEventListener("keydown", saveEventHandler);
+  }, [handleSave]);
+
+  return (
+    <>
+      <Head>
+        <title>Internationalizer</title>
+      </Head>
+      <div className="app">
+        <FileListing
+          activeFilePath={file.relativePath}
+          onFileSelected={handleFileSelected}
+        />
+
+        <div className="preview">
+          {file.path && (
+            <ToolBar
+              hasPendingChanges={hasPendingChanges}
+              onSave={handleSave}
+            />
+          )}
+
+          <main>
+            {file.path ? (
+              <EditTable
+                translations={file.translations}
+                onChange={handleChange}
+              />
+            ) : (
+              <p style={{ textAlign: "center" }}>
+                Start tranlating by selecting file in the left
+              </p>
+            )}
+          </main>
+        </div>
+      </div>
+    </>
+  );
+};
+
+const handlePendingChanges = (event) => {
+  event.returnValue = "";
+};
+
+export default HomePage;
